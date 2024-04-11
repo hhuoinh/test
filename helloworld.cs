b4853c19fdf3c533bf70e41ed536a1c22b99ede2
hhuoinh <166723198+hhuoinh@users.noreply.github.com>
2024-04-11 16:30:33 +0300

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+
+    void Start()
+    {
+        print("Hello world");
+    }
+}
